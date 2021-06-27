68140f46f5e8523ecd46b223b4e1dd328a921b22
Bill <billsong19@hotmail.com>
2021-06-27 18:49:19 +1200

    changed imported css to divs cus it was broken

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -15,19 +15,19 @@ const User = props => (
   
 export default function About() {
   return (
-    <Container>
+    <div>
       <h1>About CSS Modules</h1>
       <p>CSS Modules are cool</p>
-      <User
-        username="Jane Doe"
+      <div
+        divname="Jane Doe"
         avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
         excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
       />
-      <User
+      <div
         username="Bob Smith"
         avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
         excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
       />
-    </Container>
+    </div>
   )
 }
